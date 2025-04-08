9f95ffa0f74e0aa3575a894cf0713a9fc6b71a44
gtemate <dev.sekhmet@gmail.com>
2025-04-08 12:45:59 +0200

    initial commit

---
+++ kwatt-app/src/main/java/com/kezisoft/app/user/UserController.java
@@ -0,0 +1,16 @@
+package com.kezisoft.app.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public List<String> all() {
+        return List.of("Alice", "Bob", "Charlie");
+    }
+}
