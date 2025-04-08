e570e14e9dc79320d77c3dba57146de9eedeb789
gtemate <dev.sekhmet@gmail.com>
2025-04-08 12:37:13 +0200

    initial commit

---
+++ kwatt-app/src/main/java/com/kezisoft/app/KwattServicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.kezisoft.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwattServicesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwattServicesApiApplication.class, args);
+	}
+
+}
